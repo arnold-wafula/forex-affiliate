9b0c6b3eb0319b2c395f5eb9d47c062f25663448
Arnold Wafula <arnold.wafula0@gmail.com>
2025-09-30 16:24:28 +0300

    Moved the pages to the app folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,12 @@
-import Navbar from "@/components/site/Navbar"
 import Hero from "@/components/site/Hero"
 import Features from "@/components/site/Features"
 import Instructions from "@/components/site/Instructions"
 import Testimonials from "@/components/site/Testimonials"
 import CTA from "@/components/site/CTA"
-import Footer from "@/components/site/Footer"
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
-      {/* Navbar */}
-      {/* <Navbar /> */}
-
       {/* Main content */}
       <main className="flex-1">
         <Hero />
@@ -20,9 +15,6 @@ export default function Home() {
         <Testimonials />
         <CTA />
       </main>
-
-      {/* Footer */}
-      {/* <Footer /> */}
     </div> 
   )
 }
